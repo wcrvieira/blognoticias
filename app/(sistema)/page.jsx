b8ee21d04f1d4f8cd7a5498558d3b7ea2bde2626
drifortunato <adrianofortunatomachado@hotmail.com>
2023-04-22 14:16:50 -0300

    atualizacao#2

--- app/(sistema)/page.jsx
+++ app/(sistema)/page.jsx
@@ -5,7 +5,24 @@ export const metadata = {
 export default function Page() {
     return (
         <>
-            Exemplo de Projeto - Home
+            <br />
+            <div class="card">
+                <h5 class="card-header">00/00/000</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Título da Notícia</h5>
+                    <p class="card-text">texto da noticia</p>
+                    <a href="#" class="btn btn-primary">Continuar lendo...</a>
+                </div>
+            </div>
+            <br />
+            <div class="card">
+                <h5 class="card-header">00/00/000</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Título da Notícia</h5>
+                    <p class="card-text">texto da noticia</p>
+                    <a href="#" class="btn btn-primary">Continuar lendo...</a>
+                </div>
+            </div>            
         </>
     )
 }
